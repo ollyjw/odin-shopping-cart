ea54df180feb5d0f5d12638ab61f83fd66efe662
ollyjw <owilcocks@gmail.com>
2024-04-09 14:16:53 +0100

    nav dropdown changes products acc to category

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import CardsContainer from "./CardsContainer";
 
 const Shop = () => {
   const [storeData, setStoreData] = useState([]);
+  const { category } = useParams();
   
   useEffect(() => {
     async function fetchStoreProducts() {
-      const response = await fetch('https://fakestoreapi.com/products/', {mode: 'cors'});
+      let response;
+
+      if (category == undefined) {
+        response = await fetch('https://fakestoreapi.com/products/', {mode: 'cors'});
+      } else {
+        response = await fetch(`https://fakestoreapi.com/products/category/${category}`);
+      }
+
       const storeProducts = await response.json();
       console.log(storeProducts);
       setStoreData(storeProducts);
@@ -15,7 +23,7 @@ const Shop = () => {
     }
 
     fetchStoreProducts();
-  }, [])
+  }, [category]);
 
   return (
     <>
@@ -29,5 +37,3 @@ const Shop = () => {
 };
 
 export default Shop;
-
-// NOTES - could go Home > Shop > Categories > Products

96484dc0d1339b5e512decdaef484939d198b5b6
ollyjw <owilcocks@gmail.com>
2024-07-03 11:55:44 +0100

    added mobile navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,33 +7,54 @@ import { CartContext } from "../contexts/CartContext";
 const Header = ({ categories }) => {
   const {totalItems} = useContext(CartContext);
 
+  const hamburger = document.querySelector(".hamburger");
+  const body = document.querySelector("body");
+  const navMenu = document.querySelector(".nav-menu");
+
+  function mobileMenu() {
+    hamburger.classList.toggle("menu-active");
+    navMenu.classList.toggle("menu-active");
+    body.classList.toggle("no-scroll");
+  }
+
+  function closeMenu() {
+    hamburger.classList.remove("menu-active");
+    navMenu.classList.remove("menu-active");
+    body.classList.remove("no-scroll");
+}
+
   return (
     <>
       <div className="header">
         <nav>
-          <Link to="/" className="home-logo nav-link">
+          <Link to="/" className="home-logo nav-link" onClick={closeMenu}>
             <h2>FAKE STORE</h2>
-          </Link>
-          <ul>
+          </Link>          
+          <ul className="nav-menu">
             <li>
-              <Link to="/" className="nav-link">Home</Link>
+              <Link to="/" className="nav-link" onClick={closeMenu}>Home</Link>
             </li>
             <li className="dropdown">
-              <Link to="shop" className="nav-link dropdown-link">Shop &gt;</Link>
+              <Link to="shop" className="nav-link dropdown-link" onClick={closeMenu}>Shop <span className="lg-screen">&gt;</span></Link>
               <div className="dropdown-links">
                 {categories.map((category) => (
-                  <Link key={category} to={`categories/${category}`} className="nav-link">
+                  <Link key={category} to={`categories/${category}`} className="nav-link" onClick={closeMenu}>
                     {category}
                   </Link>
                 ))}
               </div>
             </li>
             <li>
-              <Link to="cart" className="nav-link cart-link">
+              <Link to="cart" className="nav-link cart-link" onClick={closeMenu}>
                 <img src="/src/assets/shopping-cart.svg" alt="Cart" className="cart-icon"/> <span className="cart-quantity">{totalItems}</span>
               </Link>
             </li>
           </ul>
+          <button className="hamburger" alt="menu" onClick={mobileMenu}>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </button>
         </nav>
       </div>
     </>

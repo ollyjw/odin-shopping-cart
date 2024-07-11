86b02a5a8845a7e341f961c5e5e865bc43e3bc38
ollyjw <owilcocks@gmail.com>
2024-07-11 16:04:20 +0100

    commented console.log

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
         { mode: "cors" }
       );
       const storeCategories = await response.json();
-      console.log(storeCategories);
+      // console.log(storeCategories);
       setStoreCategoryData(storeCategories);
       return storeCategories;
     }
@@ -25,7 +25,7 @@ const App = () => {
     <div className="app">
       <Header categories={storeCategoryData} />
       <Outlet />
-    </div>    
+    </div>
   );
 };
 

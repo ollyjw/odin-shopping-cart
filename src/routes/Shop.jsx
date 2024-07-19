a980e1896fd49b08ab8c5e287a39b4b25588f98f
ollyjw <owilcocks@gmail.com>
2024-07-19 16:14:58 +0100

    fix for banner background images

--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -3,6 +3,10 @@ import { useParams } from "react-router-dom";
 import CardsContainer from "../components/CardsContainer";
 import { ProductCard } from "../components/Card";
 import { ToastContainer } from "react-toastify";
+import electronicsImg from "../assets/img/electronics.jpg";
+import jeweleryImg from "../assets/img/jewelery.jpg";
+import mensClothingImg from "../assets/img/mensclothing.jpg";
+import womensClothingImg from "../assets/img/womensclothing.jpg";
 import "react-toastify/dist/ReactToastify.css";
 import "../styles/shop.css";
 
@@ -12,6 +16,8 @@ const Shop = () => {
 
   const isMobile = window.innerWidth <= 600;
   const toastLimit = isMobile ? "3" : "6";
+
+  console.log(category);
 
   useEffect(() => {
     async function fetchStoreProducts() {
@@ -48,15 +54,39 @@ const Shop = () => {
           >
             <h1>SHOP</h1>
           </div>
-        ) : (
+        ) : category === "electronics" ? (
           <div 
             className="banner"
             id={category.replace(/'| /g, '')}
-            style={{backgroundImage: `linear-gradient(rgba(31, 80, 98, 0.8), rgba(31, 80, 98, 0.8)), url(/src/assets/img/${category.replace(/'| /g, '')}.jpg)`}}
+            style={{backgroundImage: `linear-gradient(rgba(31, 80, 98, 0.8), rgba(31, 80, 98, 0.8)), url(${electronicsImg})`}}
           >
             <h1>{category.charAt(0).toUpperCase() + category.slice(1)}</h1>
           </div>
-        )}
+        ) : category === "jewelery" ? (
+          <div 
+            className="banner"
+            id={category.replace(/'| /g, '')}
+            style={{backgroundImage: `linear-gradient(rgba(31, 80, 98, 0.8), rgba(31, 80, 98, 0.8)), url(${jeweleryImg})`}}
+          >
+            <h1>{category.charAt(0).toUpperCase() + category.slice(1)}</h1>
+          </div>
+        ) : category === "men's clothing" ? (
+          <div 
+            className="banner"
+            id={category.replace(/'| /g, '')}
+            style={{backgroundImage: `linear-gradient(rgba(31, 80, 98, 0.8), rgba(31, 80, 98, 0.8)), url(${mensClothingImg})`}}
+          >
+            <h1>{category.charAt(0).toUpperCase() + category.slice(1)}</h1>
+          </div>
+        ) : category === "women's clothing" ? (
+          <div 
+            className="banner"
+            id={category.replace(/'| /g, '')}
+            style={{backgroundImage: `linear-gradient(rgba(31, 80, 98, 0.8), rgba(31, 80, 98, 0.8)), url(${womensClothingImg})`}}
+          >
+            <h1>{category.charAt(0).toUpperCase() + category.slice(1)}</h1>
+          </div>
+        ) : null}
         <CardsContainer storeData={storeData} product={ProductCard} />
       </div>
     </>
